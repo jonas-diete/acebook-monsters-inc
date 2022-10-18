b3e852dfe2450c0435cdd9d666fab13fdde718de
Guillermina Lorenzo Martinez <guillerminalorenzomartinez@MASTUDE5H47Q6L4.localdomain>
2022-10-18 10:55:24 +0100

    Change profile picture in progress

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const UsersController = require("../controllers/users");
 router.get("/new", UsersController.New);
 router.post("/new", UsersController.Create);
 router.get("/account", UsersController.View);
+router.post("/account/image", UsersController.Image);
 
 
 module.exports = router;
