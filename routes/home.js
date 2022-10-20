8225a8df38b57deab7f945b3413bd0e3d469bf12
Liovirgilda Mendonca <liovirgilda_developer@gmail.com>
2022-10-20 17:49:40 +0100

    about us page

--- routes/home.js
+++ routes/home.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const HomeController = require("../controllers/home");
 
 router.get("/", HomeController.Index);
+router.get('/about', HomeController.About);
 
 module.exports = router;
