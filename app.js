bfeca25014d696f6eef6bd95e6b99c41bfd1d175
Jonas Diete <jonas.diete@gmail.com>
2022-11-15 16:57:55 +0000

    ready for upload to heroku

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ const sessionChecker = (req, res, next) => {
 app.use(bodyParser.json());
 
 // Mongo URI test or development
-const password = encodeURIComponent("15081922");
+const password = encodeURIComponent("//");
 
 const mongoDbUrl = process.env.MONGODB_URL || `mongodb+srv://makers:${password}@learning.vltc2du.mongodb.net/?retryWrites=true&w=majority`;
 const conn = mongoose.createConnection(mongoDbUrl);
